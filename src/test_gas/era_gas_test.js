30765ffb0b130d0cebc08032a6f8f61f1a9e997f
Peter Robinson <drinkcoffee@eml.cc>
2018-08-28 13:15:50 +1000

    Ensured each set of gas measurements used new addresses

--- src/test_gas/era_gas_test.js
+++ src/test_gas/era_gas_test.js
@@ -13,6 +13,25 @@
 /**
  * Tests which analyse gas usage of the Ethereum Registration Authority contract.
  *
+ * The following is the gas usage shown when the tests are run:
+ *
+ * gas: contract deploy: 961758
+ * gas: add domain: 87663
+ * gas: add domain (same domain): 42419
+ * gas: add domain (same domain): 42419
+ * gas: add domain (none): 25104
+ * gas: add domain (none): 25104
+ * gas: add domain (none): 25104
+ * gas: add domain (auth): 30449
+ * gas: add domain (auth): 30449
+ * gas: add domain (auth): 30449
+ * gas: add domain (org): 30513
+ * gas: add domain (org): 30513
+ * gas: add domain (domain): 31665
+ * gas: add domain (domain): 31665
+ * gas: removeDomain (existing): 20099
+ * gas: removeDomain (not existing): 24237
+ * gas: removeDomain (not existing): 24237
  */
 const ERAImplementation = artifacts.require("./ERA_v1.sol");
 const ERAImplementation2 = artifacts.require("./ERA_v2.sol");
@@ -26,44 +45,49 @@ contract('ERA gas test', function(accounts) {
     const testAuthAddress1 = "0x0000000000000000000000000000000000000021";
     const testOrgInfoAddress1 = "0x0000000000000000000000000000000000000123";
 
-    const testDomainHash1 = "0x101";
     const domainOwner = accounts[1];
 
 
 
     async function tests(eraInterface) {
+        // Store and update different domains hashes each time, thus
+        // ensuring new data locations are written to.
+        let testDomainHash1 = "0x101";
         let result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, testOrgInfoAddress1, domainOwner);
-        console.log("gas: add domain: " + result.receipt.gasUsed);
+        console.log("add domain (auth, domainInfo, domainOwner) First Write: " + result.receipt.gasUsed);
         result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, testOrgInfoAddress1, domainOwner, {from:domainOwner});
-        console.log("gas: add domain (same domain): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, testOrgInfoAddress1, domainOwner, {from:domainOwner});
-        console.log("gas: add domain (same domain): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (none): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (none): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (none): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (auth): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (auth): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, DONT_SET, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (auth): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, testOrgInfoAddress1, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (org): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, testOrgInfoAddress1, DONT_SET, {from:domainOwner});
-        console.log("gas: add domain (org): " + result.receipt.gasUsed);
+        console.log("add domain (auth, domainInfo, domainOwner) Subsequent Writes: " + result.receipt.gasUsed);
+
+        testDomainHash1 = "0x102";
+        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, DONT_SET);
+        console.log("add domain (none) First Write: " + result.receipt.gasUsed);
+        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, DONT_SET);
+        console.log("add domain (none) Subsequent Writes: " + result.receipt.gasUsed);
+
+        testDomainHash1 = "0x103";
+        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, DONT_SET, DONT_SET);
+        console.log("add domain (auth) First Write: " + result.receipt.gasUsed);
+        result = await eraInterface.addUpdateDomain(testDomainHash1, testAuthAddress1, DONT_SET, DONT_SET);
+        console.log("add domain (auth) Subsequent Writes: " + result.receipt.gasUsed);
+
+        testDomainHash1 = "0x104";
+        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, testOrgInfoAddress1, DONT_SET);
+        console.log("add domain (domainInfo) First Write: " + result.receipt.gasUsed);
+        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, testOrgInfoAddress1, DONT_SET);
+        console.log("add domain (domainInfo) Subsequent Writes: " + result.receipt.gasUsed);
+
+        testDomainHash1 = "0x105";
+        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, domainOwner);
+        console.log("add domain (domainOwner) First Write: " + result.receipt.gasUsed);
         result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, domainOwner, {from:domainOwner});
-        console.log("gas: add domain (domain): " + result.receipt.gasUsed);
-        result = await eraInterface.addUpdateDomain(testDomainHash1, DONT_SET, DONT_SET, domainOwner, {from:domainOwner});
-        console.log("gas: add domain (domain): " + result.receipt.gasUsed);
+        console.log("add domain (domainOwner) Subsequent Writes: " + result.receipt.gasUsed);
+
+        // Point to the domain with auth, domain info, and domain owner set.
+        testDomainHash1 = "0x101";
         result = await eraInterface.removeDomain(testDomainHash1);
-        console.log("gas: removeDomain (existing): " + result.receipt.gasUsed);
+        console.log("removeDomain (when domain in use): " + result.receipt.gasUsed);
         result = await eraInterface.removeDomain(testDomainHash1);
-        console.log("gas: removeDomain (not existing): " + result.receipt.gasUsed);
-        result = await eraInterface.removeDomain(testDomainHash1);
-        console.log("gas: removeDomain (not existing): " + result.receipt.gasUsed);
+        console.log("removeDomain (when domain not in use): " + result.receipt.gasUsed);
 
         await common.dumpAllDomainAddUpdateEvents(eraInterface);
     }
