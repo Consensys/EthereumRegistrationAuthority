49a83d568b737951d0fda33ef8e05d5730aa9946
Peter Robinson <drinkcoffee@eml.cc>
2018-11-21 13:52:03 +1000

    Initial gradle project

---
+++ wrappers/java/src/main/java/tech/pegasys/era/EthereumRegistrationAuthority.java
@@ -0,0 +1,21 @@
+package tech.pegasys.era;
+
+public class EthereumRegistrationAuthority {
+
+
+    // params: Web3j instance and a wallet
+    public EthereumRegistrationAuthority() {
+
+    }
+
+
+
+    public void addDomain(String domainName) {
+
+    }
+
+    public void updateDomain() {
+
+    }
+
+}
---
+++ wrappers/java/src/main/java/tech/pegasys/era/EthereumRegistrationAuthorityFactory.java
@@ -0,0 +1,40 @@
+package tech.pegasys.era;
+
+public class EthereumRegistrationAuthorityFactory {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+    }
+
+
+
+    // params: Web3j instance and a wallet
+    public EthereumRegistrationAuthorityFactory() {
+
+    }
+
+
+    public EthereumRegistrationAuthority eraDeploy() {
+        return null;
+    }
+
+    // params: ethereum address
+    public EthereumRegistrationAuthority eraAtAddress() {
+        return null;
+    }
+
+
+    public void deployDomainInfo() {
+
+    }
+
+    public void deployFinder() {
+
+    }
+
+
+
+
+
+}
