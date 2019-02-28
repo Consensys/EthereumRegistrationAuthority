dee54412301a56c7c591d9eaf517382334ed872f
bickie343 <bickie343@gmail.com>
2019-02-28 18:28:13 +1000

    create tools package with EraViewer and EraDeploy

--- wrappers/java/src/main/java/tech/pegasys/era/tools/EraDeploy.java
+++ wrappers/java/src/main/java/tech/pegasys/era/tools/EraDeploy.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.era.eraviewer;
+package tech.pegasys.era.tools;
 
 import tech.pegasys.era.EthereumRegistrationAuthority;
 import tech.pegasys.era.EthereumRegistrationAuthorityFactory;
